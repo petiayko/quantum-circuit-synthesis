52404bee9ea80506f5ddea7e2dd21806005fae71
Peter <minmen2013@yandex.ru>
2025-05-12 01:59:51 +0300

    Implement input parser

--- include/gates.hpp
+++ include/gates.hpp
@@ -1,6 +1,15 @@
 #ifndef QUANTUM_CIRCUIT_SYNTHESIS_GATES_HPP
 #define QUANTUM_CIRCUIT_SYNTHESIS_GATES_HPP
 
+#include <string>
+
+static const std::string NOT    = "NOT";
+static const std::string CNOT   = "CNOT";
+static const std::string CCNOT  = "CCNOT";
+static const std::string kCNOT  = "kCNOT";
+static const std::string SWAP   = "SWAP";
+static const std::string CSWAP  = "CSWAP";
+
 class Gate {
 };
 
